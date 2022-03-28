86d2e3d64584f35bd7c70aa047f76a9f286a7226
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-03-29 01:07:04 +1300

    Created start_eel function and a main python file that will run the function to start the web app

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.data_collection import start_eel
+
+
+if __name__ == "__main__":
+    start_eel()
--- src/data_collection.py
+++ src/data_collection.py
@@ -3,7 +3,8 @@ from pathlib import Path, PurePath
 import eel
 
 
-script_dir = PurePath(Path(__file__).parent.resolve(), "web")
+def start_eel():
+    script_dir = PurePath(Path(__file__).parent.resolve(), "web")
 
-eel.init(script_dir)
-eel.start('index.html', mode="chrome-app")
+    eel.init(script_dir)
+    eel.start('index.html', mode="chrome-app")
