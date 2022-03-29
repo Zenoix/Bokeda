de837c6a779c8f33e357a1ac5260d916e5250f0f
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-03-29 20:28:10 +1300

    Moved the start eel function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from src.data_collection import start_eel
+from pathlib import Path, PurePath
+
+import eel
+
+
+def start_eel():
+    script_dir = PurePath(Path(__file__).parent.resolve(), "web")
+
+    eel.init(script_dir)
+    eel.start('index.html', mode="chrome-app")
 
 
 if __name__ == "__main__":
--- src/data_collection.py
+++ src/data_collection.py
@@ -1,10 +1,1 @@
-from pathlib import Path, PurePath
 
-import eel
-
-
-def start_eel():
-    script_dir = PurePath(Path(__file__).parent.resolve(), "web")
-
-    eel.init(script_dir)
-    eel.start('index.html', mode="chrome-app")
