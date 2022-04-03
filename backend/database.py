cb79c71ba313614636b6ea61dedec42622f745d8
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-04-04 01:37:54 +1200

    Returned the connection

--- backend/database.py
+++ backend/database.py
@@ -5,3 +5,4 @@ from pathlib import PurePath, Path
 def connect_to_database():
     location = PurePath(Path(__file__).parent.resolve(), "categories.db")
     con = sqlite3.connect(location)
+    return con
